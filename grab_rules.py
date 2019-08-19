6b437388e4c1785981a6e0198d3d423f5e872500
Andrew Villarrubia <andrew@primefour.com>
2019-08-19 00:00:30 -0500

    Update things to interface with Fryyyyy/Fryatog.

--- grab_rules.py
+++ grab_rules.py
@@ -2,10 +2,23 @@ import json
 import re
 
 rules_json = []
+examples_json = []
+
 with open("cr.txt", 'r', encoding='utf-8') as comp_rules:
     comp_rules = comp_rules.read()
-    rules = re.findall('^(\d{3}\.\d+[a-z]?)\.?\s(.*)', 
-                       comp_rules, 
+
+    comp_rules = comp_rules.replace("“", "\"")
+    comp_rules = comp_rules.replace("”", "\"")
+    comp_rules = comp_rules.replace("’", "'")
+    comp_rules = comp_rules.replace("‘", "'")
+    comp_rules = comp_rules.replace("—", "-")
+    comp_rules = re.sub(r"(\w)–—(\w)", r"\1—\2", comp_rules)
+    comp_rules = comp_rules.replace("(tm)", "™")
+    comp_rules = comp_rules.replace("(r)", "®")
+    comp_rules = re.sub(r"\n\s{4,}(\w)", r" \1", comp_rules)
+
+    rules = re.findall('^(\d{3}\.[^\s.]{1,3})[\s.]*(.*)(?:\n(Example: .*))?(?:\n(Example: .*))?(?:\n(Example: .*))?(?:\n(Example: .*))?',
+                       comp_rules,
                        re.MULTILINE)
 
     with open('rules.json', 'w', encoding='utf-8') as output:
@@ -13,3 +26,18 @@ with open("cr.txt", 'r', encoding='utf-8') as comp_rules:
             new_rule = {'ruleNumber': rule[0], 'ruleText': rule[1]}
             rules_json.append(new_rule)
         output.write(json.dumps(rules_json, indent=4))
+
+    with open('examples.json', 'w', encoding='utf-8') as examples_output:
+        examples_json = []
+        for rule in rules:
+            nonempty_examples = []
+            for ex in rule[2:5]:
+                if ex != '':
+                    nonempty_examples.append(ex)
+            actual_examples = '\n'.join(nonempty_examples)
+            if (actual_examples) == '':
+                actual_examples = "Example not found"
+            new_example = {'exampleText': actual_examples,
+                           'ruleNumber': rule[0]}
+            examples_json.append(new_example)
+        examples_output.write(json.dumps(examples_json, indent=4))
